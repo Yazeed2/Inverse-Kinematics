8ba132f4f3d0e93fab994a4fa89f60dce3dbda48
Yazeed aljuhani <yazeedmalmarwani@gmail.com>
2021-05-30 12:40:57 +0300

    edited boundries

--- boundries.py
+++ boundries.py
@@ -1,5 +1,7 @@
 from twoDOF import twoDOF
 from matplotlib import pyplot
+import numpy as np
+
 
 end = 300
 x = 0
@@ -7,18 +9,21 @@ badIdea = []
 goodIdea = []
 xcord = []
 ycord = []
+angles = []
 def possible(x,y): 
     try:
         jontAngle, tipAngle = twoDOF(x,y, 50,68)
-        if(jontAngle < 0  or tipAngle < 0):
-            return False, 'nigative'
-        else:
-            return True, None
+        # if(jontAngle < 0  or tipAngle < 0 or jontAngle > 180 or tipAngle > 180 ):
+        #     return False, 'nigative'
+        # else:
+        angles.append(jontAngle)
+        angles.append(tipAngle)
+        return True, None
     except:
         return False, 'math'
 
-for i in range(0, end+1):
-    print((i/end) * 100 ,'%')
+for i in range(-1* end, end+1):
+    print(round( (abs(i)/ (2*end)) * 100) ,'%')
     row = []
     for j in range(-1* end, end+1):
         isPossible, reason = possible(i, j)
@@ -26,5 +31,8 @@ for i in range(0, end+1):
             xcord.append(i)
             ycord.append(j)
 
-        
+print(max(angles) ) 
+print(min(angles) ) 
+print(angles)
 pyplot.scatter(xcord, ycord)
+pyplot.show()
